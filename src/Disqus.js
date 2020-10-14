1d923714952362e830edd76f58e907b883b4f6d5
Gregg Tavares <github@greggman.com>
2020-10-14 17:34:51 +0900

    comments about disqus

--- src/Disqus.js
+++ src/Disqus.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import {createURL} from './url.js';
 
+// Disqus is served from a separate domain in an iframe. In other words
+//
+// +--[jsgist.org]-----------------------------+
+// |                                           |
+// | +--[iframe src=jsgist.devcomments.org]--+ |
+// | |                                       | |
+// | | +--[iframe src=disqus.com]---+        | |
+// | | |                            |        | |
+// | | +----------------------------+        | |
+// | |                                       | |
+// | +---------------------------------------+ |
+// |                                           |
+// +-------------------------------------------+
+//
+// This is because disqus works by embedded script
+// so we can't let it be on our page directly
+// otherwise it could read our secrets
+//
 export default class Disqus extends React.Component {
   constructor() {
     super();
     this.iframeRef = React.createRef();
   }
+  // Disqus's script creates an iframe and the content in the iframe
+  // communicates back to the main page to tell it the size of the iframe
+  // so the main page can adjust the iframe size. We do that same thing
+  // so our top page can just the size of the middle iframe in the
+  // diagram above.
   handleMessage = (e) => {
     const {type, data} = e.data;
     switch (type) {
       case 'resize':
-        console.log('-got resize msg from comments.js--');
         this.iframeRef.current.style.height = `${data.height}px`;
         break;
       default:
