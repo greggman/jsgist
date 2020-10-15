87f3768a4f080788c3560fe25a54063d8f0b27ea
Gregg Tavares <github@greggman.com>
2020-10-15 18:32:40 +0900

    more embed examples

--- src/Embed.js
+++ src/Embed.js
@@ -7,8 +7,15 @@ export default function Embed(props) {
       <div>
         <p>
         You can embed a jsGist by creating an iframe pointing to
-        {' '}<code>jsgist.org/embed.html?src=&lt;src&gt;</code>{' '} where
-        {' '}<code>&lt;src&gt;</code>{' '} is one of the forms above.
+        {' '}<code>https://jsgist.org/embed.html?src=&lt;src&gt;</code>{' '} where
+        {' '}<code>&lt;src&gt;</code>{' '} is one of the forms above. Either
+        {' '}<code>?src=&lt;gist_id&gt;</code>{' '}or
+        {' '}<code>?src=&lt;base64url&gt;</code>{' '}or
+        {' '}<code>?src=&lt;url_to_json&gt;</code>{' '}.
+        You can also add {' '}<code>noheader=true</code>{' '} if you don't want the header
+        to appear. You can also just run them directly rather
+        than put in an iframe. <a target="_blank" rel="noopener noreferrer" href="https://jsgist.org/embed.html?src=bad0a8491bd6614e729ff01cc14089c9">[example]</a>{' '}
+        <a target="_blank" rel="noopener noreferrer" href={'https://jsgist.org/embed.html?src=bad0a8491bd6614e729ff01cc14089c9&noheader=true'}>[example no header]</a>
         </p>
         { gistId
           ? (
