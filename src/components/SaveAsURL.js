9d98dbc510df8afc5e64c4fa7403cc8f182b4ed5
Gregg Tavares <github@greggman.com>
2020-11-13 21:34:09 +0900

    too long url

--- src/components/SaveAsURL.js
+++ src/components/SaveAsURL.js
@@ -6,10 +6,17 @@ export default function SaveAsURL(props) {
   const {data} = props;
   const src = jsonToCompressedBase64(data);
   const url = `${window.location.origin}?src=${encodeURIComponent(src)}`;
-  return (
-    <div>
-      <div>Copy / bookmark the link below</div>
-      <a className="bookmark" href={url} target="_blank" rel="noopener noreferrer">{url}</a>
-    </div>
-  );
+  return url.length < 8000
+    ? (
+      <div>
+        <div>Copy / bookmark the link below</div>
+        <a className="bookmark" href={url} target="_blank" rel="noopener noreferrer">{url}</a>
+      </div>
+      )
+    : (
+      <div class="error-msg">
+        Too large to save as URL
+      </div>
+      )
+    ;
 }
